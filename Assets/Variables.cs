5ffa5b4e0960c00ffebf4236175c5ff36c01a58d
Alfonsosh02 <alfonsohenao08@gmail.com>
2024-04-15 22:32:59 +0200

    tarea echa

--- Assets/Variables.cs
+++ Assets/Variables.cs
@@ -8,17 +8,30 @@ using Unity.VisualScripting;
 public class Variables : MonoBehaviour// los dos puntos reflejan que eredan de una clase en particular, esto es progresivo entre clases
 {
     public int score = 0;
+    private int num1 = 1;
+    private int num2 = 2;
+    private int num3 = 3;
+    public float num4 = 4.5f;
+    public float num5 = 5.5f;
+    public float num6 = 6.6f;
 
-    void Start()
+
+    /*
+     void Start()
     {
+        
+        int score = num1 + num2 + num3 ;
         Debug.Log("Score: "+score);
     }
-    /*
+    */
+
+
     void Start()
     {
-
+        float score = num4 + num5 + num6;
+        Debug.Log("Score: " + score);
     }
-    */
+    
 
     //Ejer1: poner 3 variables privadas de tipo entero con numeros, sumarlos en score y mostrar el resultado
     //Ejer2: poner 3 variables publicas de tipo float, modificar sus valores desde el inspector y sumarlos en score2(float)
@@ -34,5 +47,5 @@ public class Variables : MonoBehaviour// los dos puntos reflejan que eredan de u
     {
         return score; 
     }*/
-    
+
 }
