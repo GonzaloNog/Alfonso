a52dfacdfb2f0929d73727ef01f00bd59270dab7
Alfonsosh02 <alfonsohenao08@gmail.com>
2024-04-16 18:35:48 +0200

    Ejercicio edad

--- Assets/Clase2.cs
+++ Assets/Clase2.cs
@@ -7,7 +7,10 @@ public class Clase2 : MonoBehaviour
     public int edad;
     void Start()
     {
-        
+        if (edad >= 18)
+            Debug.Log("Es mayor de edad");
+        else
+            Debug.Log("Es menor de edad");
     }
 
     // Update is called once per frame
