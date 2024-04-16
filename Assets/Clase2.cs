d8ee4e9e6a7bbd7af7a8a83298b6df45865b1196
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2024-04-16 13:21:34 -0300

    EjerIf

---
+++ Assets/Clase2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clase2 : MonoBehaviour
+{
+    public int edad;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Variables.cs
+++ Assets/Variables.cs
@@ -7,7 +7,6 @@ using Unity.VisualScripting;
 
 public class Variables : MonoBehaviour// los dos puntos reflejan que eredan de una clase en particular, esto es progresivo entre clases
 {
-    public int score = 0;
     private int num1 = 1;
     private int num2 = 2;
     private int num3 = 3;
@@ -16,22 +15,21 @@ public class Variables : MonoBehaviour// los dos puntos reflejan que eredan de u
     public float num6 = 6.6f;
 
 
-    /*
-     void Start()
-    {
-        
+
+
+    void Start()
+     {
         int score = num1 + num2 + num3 ;
-        Debug.Log("Score: "+score);
-    }
-    */
+       // Debug.Log("Score: "+score);
+     }
 
-
+    /*
     void Start()
     {
         float score = num4 + num5 + num6;
         Debug.Log("Score: " + score);
-    }
-    
+    }*/
+
 
     //Ejer1: poner 3 variables privadas de tipo entero con numeros, sumarlos en score y mostrar el resultado
     //Ejer2: poner 3 variables publicas de tipo float, modificar sus valores desde el inspector y sumarlos en score2(float)
