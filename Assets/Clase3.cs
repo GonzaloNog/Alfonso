b8a3cdd54eb647d177a6785c165ba2bc3baf07ac
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2024-04-17 14:37:01 -0300

    ej1

---
+++ Assets/Clase3.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clase3 : MonoBehaviour
+{
+    //Variables normales
+    public int edad;
+    public bool documento;
+    public int dinero;
+    public int trago = 8;
+    //Variables de UI
+    public TextMeshProUGUI documentoTexto;
+    void Start()
+    {
+        //documentoTexto.text = ;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
