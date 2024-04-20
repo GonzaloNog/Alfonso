694dc93b976933e8216f56719f6863d730a643fa
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2024-04-20 11:24:22 -0300

    Tomar

--- Assets/Clase3.cs
+++ Assets/Clase3.cs
@@ -10,23 +10,37 @@ public class Clase3 : MonoBehaviour
     public bool documento;
     public int dinero;
     public int trago = 8;
+    private int tragosTomados = 0;
     //Variables de UI
     public TextMeshProUGUI documentoTexto;
     public TextMeshProUGUI dineroTexto;
     public TextMeshProUGUI tragoTexto;
+    public TextMeshProUGUI tragoTomados;
 
     //Poner el color del trago en rojo si tengo menos dinero de lo que cuesta y en verde si tengo mas dinero de lo que cuesta
     void Start()
     {
-        tragoTexto.text = trago.ToString();
+        
+    }
+    public void comprarTrago()
+    {
+        if (dinero >= trago)
+        {
+
+            tragoTexto.color = Color.green;
+        }
+        else
+            tragoTexto.color = Color.red;
+    }
+}
+
+/*
+ tragoTexto.text = trago.ToString();
         dineroTexto.text = dinero.ToString();
         if (documento == true)
             documentoTexto.text = "Si";
         else
             documentoTexto.text = "No";
-        if (dinero >= trago)
-        tragoTexto.color = Color.green;
-        else
-        tragoTexto.color = Color.red;
-    }
-}
+
+
+ * */
