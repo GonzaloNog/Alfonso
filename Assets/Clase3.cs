2c743de3863cfddbf627d8bc50d9feaaf784b650
Alfonsosh02 <alfonsohenao08@gmail.com>
2024-04-20 16:50:24 +0200

    Ejercicio 4

--- Assets/Clase3.cs
+++ Assets/Clase3.cs
@@ -10,7 +10,7 @@ public class Clase3 : MonoBehaviour
     public bool documento;
     public int dinero;
     public int trago = 8;
-    private int tragosTomados = 0;
+    private int tragosTomadosNumero = 0;
     //Variables de UI
     public TextMeshProUGUI documentoTexto;
     public TextMeshProUGUI dineroTexto;
@@ -20,17 +20,36 @@ public class Clase3 : MonoBehaviour
     //Poner el color del trago en rojo si tengo menos dinero de lo que cuesta y en verde si tengo mas dinero de lo que cuesta
     void Start()
     {
-        
+        tragoTomados.text = tragosTomadosNumero.ToString();
+        dineroTexto.text = dinero.ToString();
+        tragoTexto.text = trago.ToString();
+        if (documento == true)
+            documentoTexto.text = "Si";
+        else
+            documentoTexto.text = "No";
     }
     public void comprarTrago()
     {
         if (dinero >= trago)
         {
-
+            dinero -= trago;
+            tragosTomadosNumero++;
             tragoTexto.color = Color.green;
+            tragoTomados.text = tragosTomadosNumero.ToString();
+            dineroTexto.text = dinero.ToString();
         }
         else
             tragoTexto.color = Color.red;
+    }
+    public void Prestamo()
+    {
+        if (dinero < trago)
+        {
+            dinero += 100;
+            dineroTexto.text = dinero.ToString();
+        }
+        else
+            Debug.Log("Tienes dinero de sobra");
     }
 }
 
